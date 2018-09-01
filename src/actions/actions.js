0921d61c36391611a77c3668acd2dbdbc0eaf7c1
Lewis Horwood <horwood.lewis@gmail.com>
2018-09-01 14:02:09 +0900

    add DELETE_TODO action

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -8,3 +8,14 @@ export function addTodo(text) {
         }
     }
 }
+
+export const DELETE_TODO = 'DELETE_TODO';
+
+export function deleteTodo(id){
+    return {
+        type: DELETE_TODO,
+        payload: {
+            id
+        }
+    }
+}
